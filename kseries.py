9f36ce2a245dfe1aac15aae795cc57286ca0e3f8
ridnarong <ridnarongp@yahoo.com>
2017-04-19 17:10:18 +0700

    การบ้าน

--- kseries.py
+++ kseries.py
@@ -1,8 +1,10 @@
 import requests
 import re
-
-web_response = requests.get('http://www.kseries.co/clip/49913/')
+# โจทย์ คือ เข้าไปหา ลิงก์ ของทุก ep ใน series เรื่องนั้นทั้งหมด ตาม url ด้านล่าง 
+# ลอง view source หรือ inspect element เพื่อดูโครงสร้างของหน้าเว็บเพจ
+web_response = requests.get('http://www.kseries.co/rebel-hong-gil-dong/')
 text_result = web_response.text
+# แก้ไข regular expression โดย ศึกษาจาก http://rubular.com/ ว่าจะเอาลิงก์มาได้ยังไง
 matches = re.search('iframe.*src=[\'|\"](\S+)[\'|\"]', text_result)
 
 for match in matches.groups():
